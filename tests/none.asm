0442857b65fb504441f4e10e6498c792454faf87
chaim <cgruda@gmail.com>
2021-12-19 23:07:46 +0200

    apply gitignore
    - remove all txt files
    - remove assemebler
    - remove example
    - remove tests

--- tests/none.asm
+++
@@ -1,5 +0,0 @@
-	halt $zero, $zero, $zero, 0
-	halt $zero, $zero, $zero, 0
-	halt $zero, $zero, $zero, 0
-	halt $zero, $zero, $zero, 0
-	halt $zero, $zero, $zero, 0
--- tests/test_lw.asm
+++
@@ -1,7 +0,0 @@
-	lw   $r3,   $zero, $imm,  1 	 # r[3]=mem[1]
-	add $zero, $zero, $zero, 0
-	lw   $r4,   $zero, $imm,  2 	 # r[4]=mem[2]
-	halt $zero, $zero, $zero, 0
-	halt $zero, $zero, $zero, 0
-	halt $zero, $zero, $zero, 0
-	halt $zero, $zero, $zero, 0
